039536e9eb238429d6e3f800b502f3346bf29d3f
mugilavathi <mugirajan95@gmail.com>
2022-12-20 10:50:43 +0530

    add my file

---
+++ main.js
@@ -0,0 +1,5 @@
+let mainarray=["name","age","degree","contact"];
+let user=parseInt(prompt("enter index"));
+let change=prompt("enter element");
+    mainarray[user]=change;
+    document.write(mainarray);
